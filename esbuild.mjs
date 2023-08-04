ed769cb45ee97f622c037b25d245d9bd03a3fce9
Washington Irving <iamwashingyou@gmail.com>
2023-08-05 01:19:07 +0300

    disable minification

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,7 +3,7 @@ import * as esbuild from 'esbuild'
 await esbuild.build({
 	entryPoints: ['./src/index.ts'],
 	bundle: true,
-	minify: true,
+	minify: false,
 	platform: 'node',
 	target: ['node16.0'],
 	outfile: './dist/index.js',
