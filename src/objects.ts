97d18c2b56568af0eef1941797e0f588def403c8
Washington Irving <iamwashingyou@gmail.com>
2023-08-05 01:46:58 +0300

    fix indentation

--- src/objects.ts
+++ src/objects.ts
@@ -2,10 +2,10 @@ import { TSESTree } from '@typescript-eslint/utils';
 import { RuleContext } from '@typescript-eslint/utils/dist/ts-eslint';
 
 export type FunctionesqueNode = TSESTree.FunctionDeclaration
-| TSESTree.FunctionExpression
-| TSESTree.ArrowFunctionExpression
-| TSESTree.TSEmptyBodyFunctionExpression
-| TSESTree.TSFunctionType
-| TSESTree.TSMethodSignature;
+	| TSESTree.FunctionExpression
+	| TSESTree.ArrowFunctionExpression
+	| TSESTree.TSEmptyBodyFunctionExpression
+	| TSESTree.TSFunctionType
+	| TSESTree.TSMethodSignature;
 
 export type MaxOptionalParamsContext = Readonly<RuleContext<'tooManyOptionalParams', [number]>>;
